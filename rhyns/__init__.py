748b75085e3b41378e67969bf427b4f4ec421057
sgirones <salvador.girones@abiquo.com>
2011-04-29 13:07:15 +0200

    Views basic structure

--- rhyns/__init__.py
+++ rhyns/__init__.py
@@ -10,8 +10,13 @@ def main(global_config, **settings):
     initialize_sql(engine)
     config = Configurator(settings=settings)
     config.add_static_view('static', 'rhyns:static')
-    config.add_route('home', '/', view='rhyns.views.my_view',
-                     view_renderer='templates/mytemplate.pt')
+    config.add_route('home', '/', view='rhyns.views.home',
+                     view_renderer='string')
+    config.add_route('status', '/thunder/{thunder_num}/status', view='rhyns.views.status', view_renderer='string')
+    config.add_route('install', '/thunder/{thunder_num}/install/{hypervisor}', view='rhyns.views.install', view_renderer='string')
+    config.add_route('poweron', '/thunder/{thunder_num}/poweron', view='rhyns.views.poweron', view_renderer='string')
+    config.add_route('poweroff', '/thunder/{thunder_num}/poweroff', view='rhyns.views.poweroff', view_renderer='string')
+    config.add_route('reboot', '/thunder/{thunder_num}/reboot', view='rhyns.views.reboot', view_renderer='string')
     return config.make_wsgi_app()
 
 
--- rhyns/views.py
+++ rhyns/views.py
@@ -1,7 +1,21 @@
 from rhyns.models import DBSession
 from rhyns.models import MyModel
+from pyramid.response import Response
 
-def my_view(request):
-    dbsession = DBSession()
-    root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
-    return {'root':root, 'project':'rhyns'}
+def home(request):
+    return Response("Waiting for Abel template :)")
+
+def status(request):
+    return Response("Status of thunder {0}".format(request.matchdict["thunder_num"]))
+
+def install(request):
+    return Response("Installing hypervisor {0} on thunder {1}...".format(request.matchdict["hypervisor"], request.matchdict["thunder_num"]))
+
+def poweron(request):
+    return Response("Poweron thunder {0}".format(request.matchdict["thunder_num"]))
+
+def poweroff(request):
+    return Response("Poweroff thunder{0}".format(request.matchdict["thunder_num"]))
+
+def reboot(request):
+    return Response("Rebooting thunder {0}".format(request.matchdict["thunder_num"]))
